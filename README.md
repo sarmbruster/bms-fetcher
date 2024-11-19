ab5a74687e51725928e0754daa8aaac78e98b7dc
Stefan Armbruster <stefan@armbruster-it.de>
2024-11-19 17:13:19 +0100

    using requests and beautifulsoup

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 A tool to fetch the CSV file from https://www.bms-fw.bayern.de/Navigation/Public/LastMinute.aspx.
 
-Since there is no direct simple way to download the csv file for that page we're using [Playwright](https://playwright.dev/python/) to simulate a browser and click on 'export' button.
+Unfortunatly that web page does not deliver the csv data directly.
+Instead you need to first fetch the main page, which contains a couple of hidden form fields.
+Those values need to put as payload together with `__EVENTTARGET=ctl00$ctl00$CM$CM$CtrlCourses$CtrlCoursesList$CtrlGrid$ctl11$ctl09` in a HTTP POST request.
 
-## Installation
+The second request delivers the csv file finally.
+
+## Installation & Usage
 
 ```bash
 poetry install
-poetry run playwright install chromium
+poetry run ./fetch.py
 ```
--- fetch.py
+++ fetch.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
-from playwright.sync_api import sync_playwright
+
+from bs4 import BeautifulSoup
+import requests
+# import logging
+
+# # These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# # The only thing missing will be the response.body which is not logged.
+# try:
+#     import http.client as http_client
+# except ImportError:
+#     # Python 2
+#     import httplib as http_client
+# http_client.HTTPConnection.debuglevel = 1
+
+# # You must initialize logging, otherwise you'll not see debug output.
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
 
 def main():
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto("https://www.bms-fw.bayern.de/Navigation/Public/lastminute.aspx")
+    s = requests.Session()
+    r = s.get("https://www.bms-fw.bayern.de/Navigation/Public/LastMinute.aspx")
+    if (r.status_code != 200):
+        raise RuntimeError("Error: Could not fetch page")
 
-        with page.expect_download() as download_info:
-            page.get_by_role("link", name="Export", exact=True).click()
-        download = download_info.value
-        download.save_as(download.suggested_filename)
-        browser.close()
+    html = BeautifulSoup(r.content, "html.parser")
+    hidden_inputs = html.find_all("input", type="hidden")
+    hidden_values = {input_tag.get("name"): input_tag.get("value") for input_tag in hidden_inputs}
+    hidden_values["__EVENTTARGET"] ='ctl00$ctl00$CM$CM$CtrlCourses$CtrlCoursesList$CtrlGrid$ctl11$ctl09'
+    csv = s.post("https://www.bms-fw.bayern.de/Navigation/Public/LastMinute.aspx", data=hidden_values)
+    if (csv.status_code != 200):
+        raise RuntimeError("Error: Could not fetch csv")    
+    print(csv.text)
 
 if __name__ == "__main__":
     main()
